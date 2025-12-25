133d06de1983cdc8a7fc3c3031218dbf1e873021
tikakurniasari28 <ntikakur@gmail.com>
2025-12-25 10:27:30 +0800

    second commit

--- automate_ni_luh_made_tika_kurniasari.py
+++ automate_ni_luh_made_tika_kurniasari.py
@@ -2,18 +2,17 @@ from pathlib import Path
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
-BASE_DIR = Path(__file__).resolve().parents[1]
+BASE_DIR = Path(__file__).resolve().parent
 
 def preprocess_data():
     input_path = BASE_DIR / "data/raw/day_wise.csv"
-    output_path = BASE_DIR / "data/processed/day_wise_processed.csv"
+    output_path = BASE_DIR / "day_wise_processed.csv"
 
     df = pd.read_csv(input_path)
 
     df = df.drop(columns=["Date"], errors="ignore")
 
     df = df.ffill()
-
     df = df.drop_duplicates()
 
     scaler = MinMaxScaler()
@@ -21,9 +20,9 @@ def preprocess_data():
 
     df_processed = pd.DataFrame(scaled, columns=df.columns)
 
-    output_path.parent.mkdir(parents=True, exist_ok=True)
-
     df_processed.to_csv(output_path, index=False)
+
+    print("Berhasil! Output:", output_path)
 
 if __name__ == "__main__":
     preprocess_data()
